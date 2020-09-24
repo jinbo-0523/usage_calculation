32490268fe5f9979c7bea680b0421400d092fc56
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-09-24 17:06:19 +0900

    rank CRUD

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,17 +1,17 @@
 class RanksController < ApplicationController
-
+  before_action :set_rank, only: %i[edit update destroy]
   def index
     @ranks = current_company.ranks.order(id: :asc)
   end
 
   def new
-    @rank = Rank.new
+    @rank = current_company.ranks.new
   end
 
   def create
-    @rank = Rank.new(rank_params)
+    @rank = current_company.ranks.new(rank_params)
     if @rank.save
-      reidirect_to ranks_path,noteci:"新しく職位を作成しました"
+      redirect_to ranks_path,noteci:"新しく職位を作成しました"
     else
       flash.now[:alert] = "作成に失敗しました"
       render :new
@@ -22,28 +22,29 @@ class RanksController < ApplicationController
   end
 
   def edit
-    @rank.find(params[:id])
-
   end
 
   def update
-    @rank.find(params[:id])
-    if @rank.save
-      reidirect_to @rank,noteci:"職位を編集しました"
+    if @rank.update(rank_params)
+      redirect_to ranks_path, notice:"職位を編集しました"
     else
       flash.now[:alert] = "編集に失敗しました"
-      render :new
+      render :edit
     end
   end
 
-  def destory
-    @rank.find(params[:id])
+  def destroy
+    @rank.destroy!
+    redirect_to ranks_path, alert: "削除しました"
+  end
 
+  def set_rank
+    @rank = current_company.ranks.find(params[:id])
   end
 
   private
   def rank_params
-    paramas.require(:rank).permit[:name, :display]
+    params.require(:rank).permit(:name, :display)
     
   end
 end
