5dce3f2d5759f9ddb73d1f13aa616c347640df0e
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-09-25 17:16:43 +0900

    ルーティング・登録用formファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'brands/index'
-  get 'brands/new'
-  get 'brands/create'
-  get 'brands/edit'
-  get 'brands/update'
-  get 'brands/destroy'
   root to: 'companies#index'
   devise_for :companies
   resources :companies
   resources :ranks
   resources :users
-  
+  resources :brands
 end
