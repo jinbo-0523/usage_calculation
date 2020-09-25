3fdff8dacb6601627f37140787ccceb99f0f6e19
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-09-25 16:58:44 +0900

    create controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'brands/index'
+  get 'brands/new'
+  get 'brands/create'
+  get 'brands/edit'
+  get 'brands/update'
+  get 'brands/destroy'
   root to: 'companies#index'
   devise_for :companies
   resources :companies
