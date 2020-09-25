c4107e9d631bfd9547ea82f2bf46f85bbe6616f4
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-09-25 16:35:31 +0900

    user CRUD

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,8 +2,9 @@ class Company < ApplicationRecord
   has_many :brands, dependent: :destroy
   has_many :ranks, dependent: :destroy
   has_many :foods, dependent: :destroy
+  has_many :users, through: :ranks
   
-  # Include default devise modules. Others available are:
+  # Include default devise modules. Others_ available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destory'
   root to: 'companies#index'
   devise_for :companies
   resources :companies
   resources :ranks
+  resources :users
   
 end
