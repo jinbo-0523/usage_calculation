8ccf0dced7b8d9601d3513ac3b7907c4d962977d
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-09-26 13:22:35 +0900

    top画面から遷移できるようにリンクの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'shops/index'
-  get 'shops/new'
-  get 'shops/create'
-  get 'shops/edit'
-  get 'shops/update'
-  get 'shops/destroy'
   root to: 'companies#index'
   devise_for :companies
   resources :companies
   resources :ranks
   resources :users
   resources :brands
+  resources :shops
 end
