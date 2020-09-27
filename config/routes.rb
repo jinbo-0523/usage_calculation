2fbe788a14a99e1b11f05ee35bf2f00fea143508
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-09-27 10:57:48 +0900

    コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'foods/index'
+  get 'foods/new'
+  get 'foods/create'
+  get 'foods/edit'
+  get 'foods/update'
+  get 'foods/destroy'
   root to: 'companies#index'
   devise_for :companies
   resources :companies
