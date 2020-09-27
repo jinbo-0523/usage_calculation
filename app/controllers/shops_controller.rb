a7bfc6d8929c29eca57fe84eacb5d1f7b36df617
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-09-27 10:27:51 +0900

    new/editにindexの埋め込み

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,57 +1,61 @@
 class ShopsController < ApplicationController
+  before_action :get_all, only: %i[new create edit update destroy]
   before_action :set_shop, only: %i[edit update destroy]
-  def index
-    @shops = current_company.shops.order(id: :asc)
-  end
 
+  def index
+  end
+  
   def new
     @shop = current_company.shops.new
     @brands = current_company.brands.order(id: :asc)
   end
-
+  
   def create
     # 他の会社の brand_id になっている場合は例外を出す
     current_company.brands.find(params[:shop][:brand_id])
-    @shop = User.new(shop_params)
+    @shop = Shop.new(shop_params)
     if @shop.save
-      redirect_to shops_path, notice: "新しくユーザーを作成しました"
+      redirect_to new_shop_path, notice: "新しく店舗を作成しました"
     else
       flash.now[:alert] = "作成に失敗しました"
       @brands = current_company.brands.order(id: :asc)
       render :new
     end
   end
-
+  
   def show
   end
-
+  
   def edit
     @brands = current_company.brands.order(id: :asc)
-
+    
   end
-
+  
   def update
     if @shop.update(shop_params)
-      redirect_to shops_path, notice:"職位を編集しました"
+      redirect_to edit_shop_path, notice:"店舗を編集しました"
     else
       flash.now[:alert] = "編集に失敗しました"
       @brands = current_company.brands.order(id: :asc)
       render :edit
     end
   end
-
+  
   def destroy
     @shop.destroy!
-    redirect_to shops_path, alert: "削除しました"
+    redirect_to new_shop_path, alert: "削除しました"
   end
-
+  
   def set_shop
     @shop = current_company.shops.find(params[:id])
   end
-
+  
+  def get_all
+    @shops = current_company.shops.order(id: :asc)
+  end
   private
   def shop_params
-    params.require(:shop).permit(:employee_id, :brand_id, :name, :display)
+    params.require(:shop).permit(:number, :brand_id, :name, :display)
   end
 end
 
