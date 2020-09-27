1d8211aaf646aabfad658039b518bcfd7b6c373e
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-09-27 18:45:45 +0900

    投稿フォーム作成

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,10 +4,17 @@ class FoodsController < ApplicationController
   end
 
   def new
-
+    @food = current_company.foods.new
   end
-
+  
   def create
+    @food = current_company.foods.new(food_params)
+    if @food.save
+      redirect_to foods_path, notice: "食材を作成しました"
+    else
+      flash.now[:alert] = "作成に失敗しました"
+      render new
+    end
   end
 
   def edit
@@ -17,5 +24,9 @@ class FoodsController < ApplicationController
   end
 
   def destroy
+  end
+  private
+  def food_params
+    params.require(:food).permit(:id, :name, :total, :unit ,:display)
   end
 end
