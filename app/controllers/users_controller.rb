67da1b5a2cb231f1c6e7f98b8cd2375d3472745b
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-09-28 12:12:28 +0900

    ユーザー設定画面の変更・indexの埋め込み

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,18 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[edit update destroy]
-  def index
-    @users = current_company.users.order(id: :asc)
-  end
-
+  before_action :get_users, only:%i[new create edit update destroy]
+  
   def new
     @user = current_company.users.new
     @ranks = current_company.ranks.order(id: :asc)
   end
-
+  
   def create
     # 他の会社の rank_id になっている場合は例外を出す
     current_company.ranks.find(params[:user][:rank_id])
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_path, notice: "新しくユーザーを作成しました"
+      redirect_to new_user_path, notice: "新しくユーザーを作成しました"
     else
       flash.now[:alert] = "作成に失敗しました"
       @ranks = current_company.ranks.order(id: :asc)
@@ -22,35 +20,36 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-  end
-
   def edit
     @ranks = current_company.ranks.order(id: :asc)
-
+    
   end
-
+  
   def update
     if @user.update(user_params)
-      redirect_to users_path, notice:"職位を編集しました"
+      redirect_to new_user_path, notice:"職位を編集しました"
     else
       flash.now[:alert] = "編集に失敗しました"
       @ranks = current_company.ranks.order(id: :asc)
       render :edit
     end
   end
-
+  
   def destroy
     @user.destroy!
-    redirect_to users_path, alert: "削除しました"
+    redirect_to new_user_path, alert: "削除しました"
   end
-
+  
   private
-
+  
   def set_user
     @user = current_company.users.find(params[:id])
   end
-
+  
+  def get_users
+    @users = current_company.users.order(id: :asc)
+  end
+  
   def user_params
     params.require(:user).permit(:employee_id, :rank_id, :name, :display)
   end
