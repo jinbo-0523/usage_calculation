3c2c6f787d8b8469b10684efb8e24e0567b73d62
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-09-28 11:48:47 +0900

    職位設定画面の変更・indexの埋め込み

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,49 +1,53 @@
 class RanksController < ApplicationController
   before_action :set_rank, only: %i[edit update destroy]
+  before_action :get_ranks, only: %i[new create edit update destroy]
   def index
-    @ranks = current_company.ranks.order(id: :asc)
   end
-
+  
   def new
     @rank = current_company.ranks.new
   end
-
+  
   def create
     @rank = current_company.ranks.new(rank_params)
     if @rank.save
-      redirect_to ranks_path, notice: "新しく職位を作成しました"
+      redirect_to new_rank_path, notice: "新しく職位を作成しました"
     else
       flash.now[:alert] = "作成に失敗しました"
       render :new
     end
   end
-
+  
   def show
   end
-
+  
   def edit
   end
-
+  
   def update
     if @rank.update(rank_params)
-      redirect_to ranks_path, notice:"職位を編集しました"
+      redirect_to new_rank_path, notice:"職位を編集しました"
     else
       flash.now[:alert] = "編集に失敗しました"
       render :edit
     end
   end
-
+  
   def destroy
     @rank.destroy!
-    redirect_to ranks_path, alert: "削除しました"
+    redirect_to new_rank_path, alert: "削除しました"
   end
-
+  
   private
-
+  
   def set_rank
     @rank = current_company.ranks.find(params[:id])
   end
-
+  
+  def get_ranks
+    @ranks = current_company.ranks.order(id: :asc)
+  end
+  
   def rank_params
     params.require(:rank).permit(:name, :display)
   end
