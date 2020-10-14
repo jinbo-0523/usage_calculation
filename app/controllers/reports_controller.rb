0d105d6fee73c70872a5b26258d4376d5372252c
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-10-14 14:01:52 +0900

    report#create失敗時のレンダーにrecipe.nameが入らないエラーの解消

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,28 +4,16 @@ class ReportsController < ApplicationController
   end
 
   def new
-# 全ユーザー
-      @users = current_company.users.where(display: true).order(id: :asc)
-# 全ブランド
-      @brands = current_company.brands.where(display: true).order(id: :asc)
-  # ①直打ちルートで必要な情報は？
-      # ブランド検索
-        @recipes = current_company.recipes.where(display: true).order(id: :asc)
-        @brands = current_company.brands.where(display: true).order(id: :asc)
-        @q = current_company.brands.ransack(params[:q])
-        @search_brands = @q.result
-      # ブランド検索からの店舗
-        @brand_shops = Shop.where(brand_id: @search_brands.ids).order(id: :asc)
-      # ブランド検索からのレシピ
-        @brand_recipes = Recipe.where(brand_id: @search_brands.ids).order(id: :asc)
-  # ②店舗詳細ルートで必要な情報は？
-    # shopの定義。shop_idをパラメーターで送る。そこにはbrandも入ってる。
-    # 店舗の属している業態のレシピ
-    # @shop = current_company.shops.find(@shops[:id])
-    # @recipes = current_company.recipes.where(display: true).order(id: :asc)
+    @users = current_company.users.where(display: true).order(id: :asc)
+    @brands = current_company.brands.where(display: true).order(id: :asc)
+    @recipes = current_company.recipes.where(display: true).order(id: :asc)
+    @q = current_company.brands.ransack(params[:q])
+    @search_brands = @q.result
+    @brand_shops = Shop.where(brand_id: @search_brands.ids).order(id: :asc)
+    @brand_recipes = Recipe.where(brand_id: @search_brands.ids).order(id: :asc)
     @report = Report.new
     @brand_recipes.each do |recipe|
-      @report.orders.build(recipe_id: recipe.id, recipe_name: recipe.name)
+      @report.orders.build(recipe_id: recipe.id)
     end
   end
   
@@ -37,7 +25,6 @@ class ReportsController < ApplicationController
       @users = current_company.users.where(display: true).order(id: :asc)
       @brands = current_company.brands.where(display: true).order(id: :asc)
       @recipes = current_company.recipes.where(display: true).order(id: :asc)
-      @brands = current_company.brands.where(display: true).order(id: :asc)
       @q = current_company.brands.ransack(params[:q])
       @search_brands = @q.result
       @brand_shops = Shop.where(brand_id: @search_brands.ids).order(id: :asc)
@@ -55,7 +42,6 @@ class ReportsController < ApplicationController
     @users = current_company.users.where(display: true).order(id: :asc)
     @brands = current_company.brands.where(display: true).order(id: :asc)
     @recipes = current_company.recipes.where(display: true).order(id: :asc)
-    @brands = current_company.brands.where(display: true).order(id: :asc)
     @q = current_company.brands.ransack(params[:q])
     @search_brands = @q.result
     @brand_shops = Shop.where(brand_id: @search_brands.ids).order(id: :asc)
@@ -72,7 +58,6 @@ class ReportsController < ApplicationController
       @users = current_company.users.where(display: true).order(id: :asc)
       @brands = current_company.brands.where(display: true).order(id: :asc)
       @recipes = current_company.recipes.where(display: true).order(id: :asc)
-      @brands = current_company.brands.where(display: true).order(id: :asc)
       @q = current_company.brands.ransack(params[:q])
       @search_brands = @q.result
       @brand_shops = Shop.where(brand_id: @search_brands.ids).order(id: :asc)
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,4 @@
 class Order < ApplicationRecord
-  attr_accessor :recipe_name
   belongs_to :report, optional: true
   belongs_to :recipe, optional: true
   has_many :food_recipes, primary_key: :recipe_id, foreign_key: :recipe_id
