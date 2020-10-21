e82d27067637dd65ce1d2b1d764ac73944717a15
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-10-22 03:00:42 +0900

    shop/recipe/brand削除時のダイアログを修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
-  belongs_to :report, optional: true
-  belongs_to :recipe, optional: true
+  belongs_to :report
+  belongs_to :recipe
   has_many :food_recipes, primary_key: :recipe_id, foreign_key: :recipe_id
   validates :count,presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
